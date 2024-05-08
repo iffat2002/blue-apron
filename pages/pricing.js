90717c576d7a8a4402de0b9dcc7a8a5426421f14
iffat2002 <113204563+iffat2002@users.noreply.github.com>
2024-05-08 17:43:01 +0500

    pricing page

---
+++ pages/pricing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '@/components/Navbar'
+import PersonalizeMeal from '@/components/PersonalizeMeal'
+const pricing = () => {
+  return (
+    <div>
+        <Navbar />
+        <PersonalizeMeal />
+    </div>
+  )
+}
+
+export default pricing
